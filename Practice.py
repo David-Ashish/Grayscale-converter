033dfd7411f4fae500d966497a40afc249998d59
itachiuchiha <davidashish994@gmail.com>
2023-07-06 13:20:46 +0530

    Created a grayscale image conveter

--- Practice.py
+++ Practice.py
@@ -1,16 +1,21 @@
 import streamlit as st
+from PIL import Image
 
-st.title("Shiva the koja")
+st.title("Do you want to convert your image into grayscale?")
+st.write("What is your name?")
+user_name = st.text_input(label="", key= "name")
 
+with st.expander("Start Camera"):
 
-st.write("1. Is shiva gay?")
-st.checkbox("yes")
-st.checkbox('YESSSSS he is gay!!!!!!')
+    camera_image = st.camera_input("Camera")
 
+if camera_image:
 
-st.write("2. What is worlds most powerful currency?")
-st.checkbox("DOLLARS")
-st.checkbox("GYANS")
+    img = Image.open(camera_image)
+    gray_img = img.convert("L")
+    st.write("Hey", user_name,"this is your grayscale image :)")
+    st.image(gray_img)
+
 
 
 
