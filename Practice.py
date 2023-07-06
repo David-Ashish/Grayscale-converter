b090c25c006181c4df36b1c62707d2a8415d46c7
itachiuchiha <davidashish994@gmail.com>
2023-07-06 20:47:51 +0530

    Created a grayscale image conveter

--- Practice.py
+++ Practice.py
@@ -2,19 +2,27 @@ import streamlit as st
 from PIL import Image
 
 st.title("Do you want to convert your image into grayscale?")
-st.write("What is your name?")
-user_name = st.text_input(label="", key= "name")
 
+#uploaded_image = st.file_uploader("Upload Image")
 with st.expander("Start Camera"):
 
     camera_image = st.camera_input("Camera")
+
+st.write("What is your name?", font="Helvetica")
+user_name = st.text_input(label="", key="name")
 
 if camera_image:
 
     img = Image.open(camera_image)
     gray_img = img.convert("L")
-    st.write("Hey", user_name,"this is your grayscale image :)")
+    st.write("Hey modala", user_name,", i will meet you very soon :)")
     st.image(gray_img)
+
+#if uploaded_image:
+
+    #img = Image.open(uploaded_image)
+    #gray_uploaded_img = img.convert('L')
+    #st.image(gray_uploaded_img)
 
 
 
