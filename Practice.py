30ed85fd98806213ba5b27abb76d9cc3ee772aba
itachiuchiha <davidashish994@gmail.com>
2023-07-07 23:09:50 +0530

    Created a grayscale image conveter and made some changes.

--- Practice.py
+++ Practice.py
@@ -3,13 +3,14 @@ from PIL import Image
 
 st.title("Do you want to convert your image into grayscale?")
 
-#uploaded_image = st.file_uploader("Upload Image")
+st.write("What is your name?", font="Helvetica")
+user_name = st.text_input(label="", key="name")
+
 with st.expander("Start Camera"):
 
     camera_image = st.camera_input("Camera")
 
-st.write("What is your name?", font="Helvetica")
-user_name = st.text_input(label="", key="name")
+uploaded_image = st.file_uploader("Upload Image")
 
 if camera_image:
 
@@ -18,11 +19,11 @@ if camera_image:
     st.write("Hey modala", user_name,", i will meet you very soon :)")
     st.image(gray_img)
 
-#if uploaded_image:
+if uploaded_image:
 
-    #img = Image.open(uploaded_image)
-    #gray_uploaded_img = img.convert('L')
-    #st.image(gray_uploaded_img)
+    img = Image.open(uploaded_image)
+    gray_uploaded_img = img.convert('L')
+    st.image(gray_uploaded_img)
 
 
 
